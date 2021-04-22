3c19de9f5e8d98f5aafd311c09bcff1991bbbe6e
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-04-22 01:13:07 -0700

    Initial TableView Implementation

---
+++ Eulerity Async Images/Controllers/photosTableViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Eulerity Async Images
+//
+//  Created by Kenneth Dubroff on 4/22/21.
+//
+
+import UIKit
+
+class PhotosTableViewController: UITableViewController {
+    
+    var photos: [UIImage]? {
+        didSet {
+            guard photos != nil else { return }
+            tableView.reloadData()
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.photos = [CustomImage.camera.img]
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return photos?.count ?? 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // TODO: Implement with custom cell
+        guard let image = photos?[indexPath.row],
+              let cell = tableView.dequeueReusableCell(withIdentifier: "photoCell")
+        else { fatalError("Check Cell ID") }
+        
+        cell.imageView?.image = image
+        return cell
+    }
+
+
+}
+
---
+++ Eulerity Async Images/Models/EulerityImage.swift
@@ -0,0 +1,12 @@
+//
+//  EulerityImage.swift
+//  Eulerity Async Images
+//
+//  Created by Kenneth Dubroff on 4/22/21.
+//
+
+import UIKit
+
+struct EulerityImage: Codable {
+    var url: URL?
+}
