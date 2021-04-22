f5f72e71a3f315361757bf769d8a8308f113b842
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-04-22 10:33:01 -0700

    Implemented Generic Cache to improve performance

--- Eulerity Async Images/Controllers/Model Controllers/EulerityImageController.swift
+++ Eulerity Async Images/Controllers/Model Controllers/EulerityImageController.swift
@@ -8,8 +8,8 @@
 import UIKit
 
 class EulerityImageController {
-    /// row: image
-    var imageCache: [Int: UIImage] = [:]
+    /// url: imageData
+    private let imageCache = Cache<URL, Data>()
     
     private let baseURL = URL(string: "https://eulerity-hackathon.appspot.com")!
     
@@ -29,18 +29,22 @@ class EulerityImageController {
         let request = URLRequest(url: url)
         URLSession.shared.loadData(using: request) { [unowned self] data, response, error in
             guard error == nil else {
-                completion(.failure(error!))
+                DispatchQueue.main.async {
+                    completion(.failure(error!))
+                }
                 return
             }
             
             guard let response = response,
                   response.statusCode < 400 else {
                 let error = NSError(domain: "\(#file).\(#function)",
-                                    code: response?.statusCode ?? 999,
-                                    userInfo: [NSLocalizedDescriptionKey: "invalid response: \(response?.statusCode ?? 999)"])
+                                    code: 999,
+                                    userInfo: [NSLocalizedDescriptionKey: "invalid response"])
                 
-                completion(.failure(error))
-                print("invalid response in \(#function): \(response?.statusCode ?? 999)")
+                print("invalid response in \(#function)")
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
                 return
             }
             
@@ -49,22 +53,38 @@ class EulerityImageController {
                                     code: response.statusCode,
                                     userInfo: [NSLocalizedDescriptionKey: "No Data Received: \(response.statusCode)"])
                 
-                completion(.failure(error))
                 print("no data received in \(#function)")
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
                 return
             }
             
             let imageArray = self.decode(type: [EulerityImage].self, from: data)
-            completion(.success(imageArray))
+            DispatchQueue.main.async {
+                completion(.success(imageArray))
+            }
         }
-    }
-    
-    func getImage(from url: URL, completion: @escaping (Result<UIImage?, Error>) -> Void) {
-        
     }
     
     func getUploadLink(completion: @escaping (Result<UIImage?, Error>) -> Void) {
         
+    }
+    
+    func cacheImageData(_ data: Data, for url: URL) {
+        imageCache.cache(value: data, for: url)
+    }
+    
+    func getImage(for url: URL) -> UIImage? {
+        guard let data = imageCache.value(for: url) else {
+            return nil
+        }
+        
+        guard let image = UIImage(data: data) else {
+            return nil
+        }
+        
+        return image
     }
     
 }
