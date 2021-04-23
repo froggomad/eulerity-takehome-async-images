0b707b58507eb7096b83362758cdfa1c04fe6a96
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-04-22 17:07:00 -0700

    implemented passing unit test for downloading image

--- Eulerity Async Images Tests/Eulerity_Async_Images_Tests.swift
+++ Eulerity Async Images Tests/Eulerity_Async_Images_Tests.swift
@@ -10,21 +10,57 @@ import XCTest
 
 class Eulerity_Async_Images_Tests: XCTestCase {
 
-    func testGettingMetaData() {
+    func testGettingMetaData(completion: @escaping (URL?) -> Void) {
         let expectation = self.expectation(description: "Get Image Metadata")
         
         let imageController = EulerityImageController()
         
         imageController.getImageMetaData { result in
-            expectation.fulfill()
             switch result {
             case .success(let imageData):
                 XCTAssertNotNil(imageData)
+                completion(imageData?[0].url)
             case .failure(let error):
                 XCTFail(error.localizedDescription)
+                completion(nil)
             }
+            expectation.fulfill()
         }
         
+        wait(for: [expectation], timeout: 10)
+    }
+    
+    func testGettingImage() {
+        let expectation = self.expectation(description: "get image from url")
+        testGettingMetaData { (url) in
+            XCTAssertNotNil(url)
+            let imageController = EulerityImageController()
+            let fetchOp = PhotoFetchOperation(url: url!, imageController: imageController)
+            let cacheOp = BlockOperation {
+                if let data = fetchOp.imageData {
+                    imageController.cacheImageData(data, for: url!)
+                }
+            }
+            // can't cache unless we fetch first
+            cacheOp.addDependency(fetchOp)
+            
+            let queue = OperationQueue()
+            
+            queue.addOperations ([
+                fetchOp,
+                cacheOp
+            ], waitUntilFinished: false)
+            
+            let imageSetOp = BlockOperation {
+                XCTAssertNotNil(fetchOp.imageData)
+                XCTAssertNotNil(UIImage(data: fetchOp.imageData!))
+                expectation.fulfill()
+            }
+            // need to fetch before we can set
+            imageSetOp.addDependency(fetchOp)
+            
+            OperationQueue.current?.addOperation(imageSetOp)
+        }
         wait(for: [expectation], timeout: 10)
     }
 
